3cf3d80cd643bce522b7679523aa86591d0ae10b
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-10-10 16:39:16 +0200

    startup with configuration set up!

---
+++ README.md
@@ -0,0 +1,7 @@
+# To add user secrets
+
+```shell
+dotnet user-secrets init
+dotnet user-secrets set "Setting" "Setting from user-secret"
+dotnet user-secrets list
+```
---
+++ src/BlobCI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+
+namespace BlobCI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddYamlFile("appsettings.yml")
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .AddUserSecrets<Program>()
+            .Build();
+            //BlobServiceClient blobServiceClient = new BlobServiceClient(config["CONNECTION_STRING"]);
+            Console.WriteLine(config["CONNECTION_STRING"]);
+            Console.WriteLine("Test variable secret=" + config["secret"]);
+        }
+    }
+}
